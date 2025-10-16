00f659ccefe0f1d7600e114e8cd1d4ed1893b5de
25xabierif <a25xabierif@iessanclemente.net>
2025-10-16 11:03:58 +0200

    16/10 cambio 3

--- GestorZIP/src/App.java
+++ GestorZIP/src/App.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Introduce el nombre del archivoZIP que vamos a crear: ");
+        String zipName = entrada.nextLine();
+        entrada.nextLine();
+
+        System.out.println("Cuántos archivos quiere añadir al ZIP?");
+        int zipNum = entrada.nextInt();
+        entrada.nextLine();
+
+        for(int i = 0; i < zipNum ; i++){
+            System.out.println("Introduce el nombre del fichero a añadir:");
+            String nombre = entrada.nextLine();
+            entrada.nextLine();
+            System.out.println("");
+        }
+
+        entrada.close();
     }
 }
