47d99ad5bd72386153d248c542421c0b8fa573d5
25xabierif <a25xabierif@iessanclemente.net>
2026-01-26 08:46:29 +0100

    cambios semana 3 xaneiro

---
+++ NoSQL/mongodb_probas/src/main/java/probas_mongodb/Main.java
@@ -0,0 +1,9 @@
+package probas_mongodb;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+//Mediciones -> sensorld, ts(fecha), tipo("temp","hum"), valor, ubicacion
